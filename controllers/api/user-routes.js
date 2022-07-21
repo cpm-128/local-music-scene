abf658f1621f126260466d1651e39fc3c7d7d2c3
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-07-20 21:40:59 -0400

    logout api route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -76,3 +76,16 @@ router.post('/login', (req, res) => {
         });
     });
 });
+
+// LOGOUT by destroying session data
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
