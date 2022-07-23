b4ede13d0ccc2a9bffc572641cbce001132e05b6
Matt McGee <mlmcgeenc@gmail.com>
2022-07-23 14:40:15 -0400

    add sign up event listener

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,26 +1,27 @@
-async function loginHandler(e) {
-  e.preventDefault()
-  console.log('login button clicked')
+async function signupFormHandler(e) {
+	e.preventDefault();
+	console.log('signup form submitted');
 
-  const username = document.querySelector('#input-username').value.trim()
-  const password = document.querySelector('#input-password').value.trim()
+	const username = document.querySelector('#username-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
 
-  if (username && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'post',
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    })
+	if (username && password) {
+		const response = await fetch('/api/users', {
+			method: 'post',
+			body: JSON.stringify({
+				username,
+				password,
+			}),
+			headers: { 'Content-Type': 'application/json' },
+		});
 
-    if (response.ok) {
-      document.location.replace('/')
-    } else {
-      alert(response.statusText)
-    }
-  }
+		// check the response status
+		if (response.ok) {
+			console.log('success, user created');
+		} else {
+			alert(response.statusText);
+		}
+	}
 }
 
-document.querySelector('')
+document.querySelector('.create-account-form').addEventListener('submit', signupFormHandler);
