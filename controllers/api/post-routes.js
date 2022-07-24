fdb8279d57e6d9275f539debd027750e63188180
Matt McGee <mlmcgeenc@gmail.com>
2022-07-24 17:45:55 -0400

    add username to new posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -13,64 +13,58 @@ const sequelize = require('../../config/config');
 
 // GET all posts with user_id /api/posts
 router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: [
-            'id',
-            'song_title',
-            'song_url',
-            'artist_name',
-            'album_name',
-            'created_at'
-        ],
-        order: [['created_at', 'DESC']],
-        include: [
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbAllPostData => res.json(dbAllPostData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Post.findAll({
+		attributes: ['id', 'song_title', 'song_url', 'artist_name', 'album_name', 'created_at'],
+		order: [['created_at', 'DESC']],
+		include: [
+			{
+				model: User,
+				attributes: ['username'],
+			},
+		],
+	})
+		.then((dbAllPostData) => res.json(dbAllPostData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 // CREATE /api/posts
 router.post('/', (req, res) => {
-    Post.create({
-        song_title: req.body.song_title,
-        song_url: req.body.song_url,
-        artist_name: req.body.artist_name,
-        album_name: req.body.album_name,
-        user_name: req.session.user_name
-    })
-    .then(dbCreatePostData => res.json(dbCreatePostData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Post.create({
+		song_title: req.body.song_title,
+		song_url: req.body.song_url,
+		artist_name: req.body.artist_name,
+		album_name: req.body.album_name,
+		user_name: req.session.user_name,
+    user_id: req.session.user_id,
+	})
+		.then((dbCreatePostData) => res.json(dbCreatePostData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 // DELETE single post
 router.delete('/:id', (req, res) => {
-    Post.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(dbDeletePostData => {
-        if (!dbDeletePostData) {
-            res.status(404).json({ message: 'No post found with this id.' });
-            return;
-        }
-        res.json(dbDeletePostData);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+	Post.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbDeletePostData) => {
+			if (!dbDeletePostData) {
+				res.status(404).json({ message: 'No post found with this id.' });
+				return;
+			}
+			res.json(dbDeletePostData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
--- models/Post.js
+++ models/Post.js
@@ -29,7 +29,7 @@ Post.init(
 		// album_name allows null value. Independent artists are likely to release songs and demos without an assigned album
 		album_name: {
 			type: DataTypes.STRING,
-      		allowNull: true,
+			allowNull: true,
 		},
 		user_id: {
 			type: DataTypes.INTEGER,
@@ -38,6 +38,13 @@ Post.init(
 				key: 'id',
 			},
 		},
+		// user_name: {
+    //   type: DataTypes.STRING,
+		// 	references: {
+		// 		model: 'user',
+		// 		key: 'username',
+		// 	},
+		// },
 	},
 	{
 		sequelize,
@@ -47,4 +54,4 @@ Post.init(
 	}
 );
 
-module.exports = Post
+module.exports = Post;
